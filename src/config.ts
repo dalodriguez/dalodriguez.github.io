859002f5d050ea5e2367dcf212e372de234f57e0
dalodriguez <dalodriguez@gmail.com>
2024-04-02 14:10:44 +0200

    Init

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'David Lopez Rodriguez';
+export const SITE_TITLE = 'DLopez-Rodriguez';
 export const SITE_DESCRIPTION = ' ';
 
