03bd5911e3537e3ad22a9f4493ba6d4039de4354
David <david.lopezrodriguez@unil.ch>
2025-02-04 15:54:49 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://dalodriguez.github.io
 export default defineConfig({
   site: 'https://dalodriguez.github.io',
+  output: 'static',   // Ensures the output is fully static (no symlinks)
+  outDir: 'dist',     // Explicitly sets the output folder
   integrations: [mdx(), sitemap(), tailwind()]
 });
