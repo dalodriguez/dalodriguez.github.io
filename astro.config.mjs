fcd75c584c9143cb1a809a3a0363fa9a414e92ba
David <david.lopezrodriguez@unil.ch>
2025-02-07 00:06:16 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://dlopez-rodriguez.ch',
   base: '/',  
-  integrations: [mdx(), sitemap(), tailwind()]
+    integrations: [
+        mdx(), 
+        sitemap(),
+        tailwind(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 });
